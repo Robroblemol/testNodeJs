8b3b807daa589b19a7493fb7685952f6454a0a5b
Roberto Robles <robertodavidrobles@gmail.com>
2017-01-06 20:10:27 -0500

    cambio a asincrona

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
 var http = require("http"),// indico que es un tipo http
     fs = require("fs");
 
-var html = fs.readFileSync("./index.html");
-
+//var html = fs.readFileSync("./index.html");// leemos el archivo
 http.createServer(function(req,res) {
-  res.write(html);
-  res.end();
-}).listen(8080);
+ fs.readFile("./index.html",function (err,html) {
+   res.write(html);
+   res.end();
+ });// version asincrona para leer datos
+ }).listen(8080);
+
+// http.createServer(function(req,res) {
+//   res.write(html);
+//   res.end();
+// }).listen(8080);
