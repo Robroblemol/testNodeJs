59ebf4047cdb797e91dc7b306c731dd2e214fa50
Roberto Robles <robertodavidrobles@gmail.com>
2017-01-06 21:28:32 -0500

    ajustes menores

--- render_vars.js
+++ render_vars.js
@@ -5,13 +5,13 @@ var http = require("http"),// indico que es un tipo http
 
        var html_string = html.toString();// convertimos el html en cadena
        var variables = html_string.match(/[^\{\}]+(?=\})/g); // expresion regular?¿?...
+       var nombre = "Roberto";// variable a cambiar
        // nos devuelve el arreglo con lo que encontró entre llaves
        // asi que lo iteramos
        for (var i = variables.length-1; i >= 0; i--) {
          var value = eval(variables[i]);//evaluamos el string
          html_string = html_string.replace("{"+variables[i]+"}",value);
        }
-       var nombre = "Roberto";// variable a cambiar
 
        res.writeHead(200,{"Content-Type":"text/html"});// se agrega encabzados JSON
        res.write(html_string);
