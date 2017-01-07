4b94dac79026e61615e7dfd3a867c4cce4fd8e5d
Roberto Robles <robertodavidrobles@gmail.com>
2017-01-06 21:23:59 -0500

    ajuste ciclo for

--- render_vars.js
+++ render_vars.js
@@ -7,7 +7,7 @@ var http = require("http"),// indico que es un tipo http
        var variables = html_string.match(/[^\{\}]+(?=\})/g); // expresion regular?¿?...
        // nos devuelve el arreglo con lo que encontró entre llaves
        // asi que lo iteramos
-       for (var i = array.length-1; i >= 0; i--) {
+       for (var i = variables.length-1; i >= 0; i--) {
          var value = eval(variables[i]);//evaluamos el string
          html_string = html_string.replace("{"+variables[0]+"}",value);
        }
