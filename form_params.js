1ef6af224e5579d4ae6d2fc425a7feb8e87c2da9
Roberto Robles <robertodavidrobles@gmail.com>
2017-01-08 11:19:34 -0500

    ajuste en el JS para cambiar los valores

--- form_params.js
+++ form_params.js
@@ -5,20 +5,42 @@ var http = require("http"),// indico que es un tipo http
       if(req.url.indexOf("favicon.ico") > 0){
         return;// para que no siga con el resto del codigo
       }
-      //imprimimos solicitud
-      console.log("=========\n\n");
-      console.log(req);
-      console.log("=========\n\n");
+      // //imprimimos solicitud
+      // console.log("=========\n\n");
+      // console.log(req);
+      // console.log("=========\n\n");
 
      fs.readFile("./index.html",function (err,html) {
        var html_string = html.toString();// convertimos el html en cadena
        var variables = html_string.match(/[^\{\}]+(?=\})/g); // expresion regular?¿?...
        var nombre = ";"
-       //if (req.url.indexOf("?"))
+       var array_params = [];
+       var parametros = {} // hash con parametros
+       // buscamos ? quiere decir que no estan enviado parametros
+       if (req.url.indexOf("?") < 0){
+         // /?nombre  -> separamos el path que es lo que sea que va despues de /
+         var url_data = req.url.split("?");
+         // & -> se usa para separar parametros
+         var array_params = url_data[1].split("&");
+         // obtenemos: [nombre=nombre,data=loquesea]
+       }
+       for (var i = 0; i < array_params.length; i++) {
+         var param = array_params[i];
+         // nombre= Nombre;
+         var param_data = param.split("=");
+         //[nombre,Nombre]
+         parametros[param_data[0]] = param_data[1];
+         //{nombre: Nombre}
+       }
 
        for (var i = variables.length-1; i >= 0; i--) {
-         var value = eval(variables[i]);
-         html_string = html_string.replace("{"+variables[i]+"}",value);
+         //var value = eval(variables[i]);
+         //[nombre: Nombre]
+         var variable = variables[i];
+         // parametros[variable[i]]
+         //parametros[]
+         html_string = html_string.replace("{"+variables[i]+"}",parametros[variable]);
+         
        }
 
        res.writeHead(200,{"Content-Type":"text/html"});// se agrega encabzados JSON
