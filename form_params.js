bb093371879f8e82310f48297061d58820b11a5a
Roberto Robles <robertodavidrobles@gmail.com>
2017-01-09 12:58:30 -0500

    ajuste gitinore

--- form_params.js
+++ form_params.js
@@ -10,12 +10,11 @@ var http = require("http"),// indico que es un tipo http
       if(req.url.indexOf("favicon.ico") > 0){
         return;// para que no siga con el resto del codigo
       }
-
      fs.readFile("./index.html",function (err,html) {
        var html_string = html.toString();// convertimos el html en cadena
        var parametros = p(req);// llamamos la funcion que creamos
        res.writeHead(200,{"Content-Type":"text/html"});// se agrega encabzados JSON
        res.write(render(html_string,parametros));
        res.end();
-     });// version asincrona para leer datos
+      });// version asincrona para leer datos
      }).listen(8080);
