97fa72c4a81e9b4ca8419966b8b70de054d13026
Roberto Robles <robertodavidrobles@gmail.com>
2017-01-09 09:14:37 -0500

    agrega modulo render

--- form_params.js
+++ form_params.js
@@ -1,7 +1,10 @@
 var http = require("http"),// indico que es un tipo http
     fs = require("fs");
-    parser =require("./params_parse.js");// importamos el scrips
+    parser = require("./params_parse.js");// importamos el scrips
+    render = require("./render_view.js");
     var p = parser.parse;// declaramos una variables con la funcion parse
+    var render = render.render;
+
     http.createServer(function(req,res) {
       // buscamos si hay alguna incidencia con la palabra favicon
       if(req.url.indexOf("favicon.ico") > 0){
@@ -10,23 +13,9 @@ var http = require("http"),// indico que es un tipo http
 
      fs.readFile("./index.html",function (err,html) {
        var html_string = html.toString();// convertimos el html en cadena
-       var variables = html_string.match(/[^\{\}]+(?=\})/g); // expresion regular?¿?...
-       var nombre = "";
-
        var parametros = p(req);// llamamos la funcion que creamos
-
-       for (var i = variables.length-1; i >= 0; i--) {
-         //var value = eval(variables[i]);
-         //[nombre: Nombre]
-         var variable = variables[i];
-         // parametros[variable]
-         //parametros[nombre]
-         html_string = html_string.replace("{"+variables[i]+"}",parametros[variable]);
-
-       }
-
        res.writeHead(200,{"Content-Type":"text/html"});// se agrega encabzados JSON
-       res.write(html_string);
+       res.write(render(html_string,parametros));
        res.end();
      });// version asincrona para leer datos
      }).listen(8080);
---
+++ render_view.js
@@ -0,0 +1,16 @@
+function render(html,variables) {
+  var variables = html.match(/[^\{\}]+(?=\})/g); // expresion regular?¿?...
+  var nombre = "";
+
+  for (var i = variables.length-1; i >= 0; i--) {
+    //var value = eval(variables[i]);
+    //[nombre: Nombre]
+    var variable = variables[i];
+    // parametros[variable]
+    //parametros[nombre]
+    html= html.replace("{"+variables[i]+"}",parametros[variable]);
+
+  }
+  return html;
+}
+module.exports.render = render;// indicamos que lo que valos a compartir es la funcion parse
